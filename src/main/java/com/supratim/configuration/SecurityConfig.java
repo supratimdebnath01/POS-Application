413e4b959b1a26431c1c1fb48d427714f311db13
Supratim Debnath <supratimdebnath01@gmail.com>
2026-07-31 18:10:22 +0530

    App setup

---
+++ src/main/java/com/supratim/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.supratim.configuration;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.json.HTTP;
+import org.jspecify.annotations.Nullable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception{
+
+        return http
+                .sessionManagement(management ->
+                        management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize ->
+                        Authorize.requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/api/super-admin/**")
+                                .hasRole("ADMIN")
+                                .anyRequest().permitAll()
+                ).addFilterBefore(new JwtValidator(),
+                        BasicAuthenticationFilter.class
+                ).csrf(AbstractHttpConfigurer::disable)
+                .cors(
+                        cors -> cors.configurationSource(corsConfigurationSource())
+                ).build();
+    }
+}
+
+private CorsConfigurationSource corsConfigurationSource(){
+    return new CorsConfigurationSource() {
+        @Override
+        public @Nullable CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+            CorsConfiguration cfg = new CorsConfiguration();
+            cfg.setAllowedOrigins(
+                    Arrays.asList(
+                            "http://localhost:8080/"
+                    )
+            );
+            cfg.setAllowedMethods();
+            return null;
+        }
+    }
+}
